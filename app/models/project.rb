3161b05fa9985a9fa4aa9f85c592d761dba9d5d8
Aaron Sumner <asumner@mac.com>
2017-05-18 08:06:41 -0500

    Fix deprecated use of class_name option on association
    While testing scopes in the Rails console, I get the following
    deprecation warning:
    
      DEPRECATION WARNING: Passing a class to the `class_name` is deprecated
      and will raise an ArgumentError in Rails 5.2. It eagerloads more classes
      than necessary and potentially creates circular dependencies. Please
      pass the class name as a string: `belongs_to :owner, class_name: 'User'`
      (called from <class:Project> at
      /Users/asumner/code/examples/projects/app/models/project.rb:4)
    
    Fix by following the warning's guidance and passing the class name as a
    string.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :user_id }
 
-  belongs_to :owner, class_name: User, foreign_key: :user_id
+  belongs_to :owner, class_name: 'User', foreign_key: :user_id
   has_many :notes
   has_many :tasks
 end
