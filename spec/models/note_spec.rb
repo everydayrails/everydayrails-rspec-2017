eb43d74ba996464090292eda29935ad0c35b2b63
Aaron Sumner <asumner@mac.com>
2017-05-22 13:56:52 -0500

    Rearrange note model using describe, before, and context

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,59 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  it "returns notes that match the search term" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
+  describe "search message for a term" do
+    before do
+      user = User.create(
+        first_name: "Joe",
+        last_name:  "Tester",
+        email:      "joetester@example.com",
+        password:   "dottle-nouveau-pavilion-tights-furze",
+      )
 
-    project = user.projects.create(
-      name: "Test Project",
-    )
+      project = user.projects.create(
+        name: "Test Project",
+      )
 
-    note1 = project.notes.create(
-      message: "This is the first note.",
-      user: user,
-    )
-    note2 = project.notes.create(
-      message: "This is the second note.",
-      user: user,
-    )
-    note3 = project.notes.create(
-      message: "First, preheat the oven.",
-      user: user,
-    )
+      @note1 = project.notes.create(
+        message: "This is the first note.",
+        user: user,
+      )
+      @note2 = project.notes.create(
+        message: "This is the second note.",
+        user: user,
+      )
+      @note3 = project.notes.create(
+        message: "First, preheat the oven.",
+        user: user,
+      )
+    end
 
-    expect(Note.search("first")).to include(note1, note3)
-  end
+    it "returns notes that match the search term" do
+      expect(Note.search("first")).to include(@note1, @note3)
+    end
 
-  it "returns an empty collection when no results are found" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    project = user.projects.create(
-      name: "Test Project",
-    )
-
-    note1 = project.notes.create(
-      message: "This is the first note.",
-      user: user,
-    )
-    note2 = project.notes.create(
-      message: "This is the second note.",
-      user: user,
-    )
-    note3 = project.notes.create(
-      message: "First, preheat the oven.",
-      user: user,
-    )
-
-    expect(Note.search("message")).to be_empty
+    it "returns an empty collection when no results are found" do
+      expect(Note.search("message")).to be_empty
+    end
   end
 end
