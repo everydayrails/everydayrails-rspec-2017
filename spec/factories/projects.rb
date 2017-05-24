c2e9f9ff9d3d9a9c86b443ee6a333fa82fef101f
Aaron Sumner <asumner@mac.com>
2017-05-23 21:43:51 -0500

    Add factory inheritance examples for chapter 4

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,26 +4,40 @@ FactoryGirl.define do
     description "A test project."
     due_on 1.week.from_now
     association :owner
+
+    factory :project_due_yesterday do
+      due_on 1.day.ago
+    end
+
+    factory :project_due_today do
+      due_on Date.today
+    end
+
+    factory :project_due_tomorrow do
+      due_on 1.day.from_now
+    end
   end
 
-  factory :project_due_yesterday, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on 1.day.ago
-    association :owner
-  end
-
-  factory :project_due_today, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on Date.today
-    association :owner
-  end
-
-  factory :project_due_tomorrow, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on 1.day.from_now
-    association :owner
-  end
+  # Non-DRY versions ...
+  #
+  # factory :project_due_yesterday, class: Project do
+  #   sequence(:name) { |n| "Test Project #{n}" }
+  #   description "Sample project for testing purposes"
+  #   due_on 1.day.ago
+  #   association :owner
+  # end
+  #
+  # factory :project_due_today, class: Project do
+  #   sequence(:name) { |n| "Test Project #{n}" }
+  #   description "Sample project for testing purposes"
+  #   due_on Date.today
+  #   association :owner
+  # end
+  #
+  # factory :project_due_tomorrow, class: Project do
+  #   sequence(:name) { |n| "Test Project #{n}" }
+  #   description "Sample project for testing purposes"
+  #   due_on 1.day.from_now
+  #   association :owner
+  # end
 end
