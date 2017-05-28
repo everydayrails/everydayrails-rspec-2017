1d83e9fdfd5c90408f68cc950a1425eff6602537
Aaron Sumner <asumner@mac.com>
2017-05-28 14:17:55 -0500

    Add feature spec for creating projects

---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Projects", type: :feature do
+  scenario "user creates a new project" do
+    user = FactoryGirl.create(:user)
+
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect {
+      click_link "New Project"
+      fill_in "Name", with: "Test Project"
+      fill_in "Description", with: "Trying out Capybara"
+      click_button "Create Project"
+    }.to change(user.projects, :count).by(1)
+
+    expect(page).to have_content "Project was successfully created"
+    expect(page).to have_content "Test Project"
+    expect(page).to have_content "Owner: #{user.name}"
+  end
+end
