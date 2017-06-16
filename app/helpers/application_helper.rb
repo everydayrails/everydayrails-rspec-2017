6ea74bfff61dd77416a2803ba84a19be9a42674e
Aaron Sumner <asumner@mac.com>
2017-06-15 19:53:51 -0500

    Clean up user interface
    The existing user interface for the sample application isn't very
    convincing as a real-world app. Rework the interface so it seems more
    like a real application.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def full_date(date)
+    date_string = date.strftime("%B %d, %Y")
+    date_human = time_ago_in_words(date)
+    past_or_future = Time.now > date ? "ago" : "from now"
+    "#{date_string} (#{date_human} #{past_or_future})"
+  end
 end
