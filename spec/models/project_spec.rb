d1e09b6709fe387886d6973a61c8df82ecc1ffcb
Aaron Sumner <asumner@mac.com>
2017-07-24 21:40:04 -0500

    Add examples of shoulda-matchers

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,51 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "does not allow duplicate project names per user" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    user.projects.create(
-      name: "Test Project",
-    )
-
-    new_project = user.projects.build(
-      name: "Test Project",
-    )
-
-    new_project.valid?
-    expect(new_project.errors[:name]).to include("has already been taken")
-  end
-
-  it "allows two users to share a project name" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    user.projects.create(
-      name: "Test Project",
-    )
-
-    other_user = User.create(
-      first_name: "Jane",
-      last_name:  "Tester",
-      email:      "janetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    other_project = other_user.projects.build(
-      name: "Test Project",
-    )
-
-    expect(other_project).to be_valid
-  end
+  it { is_expected.to validate_uniqueness_of(:name).scoped_to(:user_id) }
 
   describe "late status" do
     it "is late when the due date is past today" do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,30 +15,10 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  it "is invalid without a first name" do
-    user = FactoryGirl.build(:user, first_name: nil)
-    user.valid?
-    expect(user.errors[:first_name]).to include("can't be blank")
-  end
-
-  it "is invalid without a last name" do
-    user = FactoryGirl.build(:user, last_name: nil)
-    user.valid?
-    expect(user.errors[:last_name]).to include("can't be blank")
-  end
-
-  it "is invalid without an email address" do
-    user = FactoryGirl.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
-
-  it "is invalid with a duplicate email address" do
-    FactoryGirl.create(:user, email: "aaron@example.com")
-    user = FactoryGirl.build(:user, email: "aaron@example.com")
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
-  end
+  it { is_expected.to validate_presence_of :first_name }
+  it { is_expected.to validate_presence_of :last_name }
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
 
   it "returns a user's full name as a string" do
     user = FactoryGirl.build(:user, first_name: "John", last_name: "Doe")
