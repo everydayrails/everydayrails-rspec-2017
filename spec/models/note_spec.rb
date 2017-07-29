97febd57c9fee3b09a09b94e6d4ebf8563ef87a4
Aaron Sumner <asumner@mac.com>
2017-07-29 05:20:20 -0500

    Add example of rspec-mocks

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -58,4 +58,11 @@ RSpec.describe Note, type: :model do
       end
     end
   end
+
+  it "delegates name to the user who created it" do
+    user = instance_double("User", name: "Fake User")
+    note = Note.new
+    allow(note).to receive(:user).and_return(user)
+    expect(note.user_name).to eq "Fake User"
+  end
 end
