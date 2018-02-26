f9dc3e292262de954be8d23d8b5e17a9569deffc
yuki3738 <y.minamiya@gmail.com>
2018-02-26 21:49:56 +0900

    5章まで

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :project do
     sequence(:name) { |n| "Project #{n}" }
-    description "A test project."
+    description "Sample project for testing purposes."
     due_on 1.week.from_now
     association :owner
 
@@ -19,6 +19,10 @@ FactoryGirl.define do
 
     trait :due_tomorrow do
       due_on 1.day.from_now
+    end
+
+    trait :invalid do
+      name nil
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
