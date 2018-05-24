4c322c660f2756a3dbb6b93adbccdba8da41bc9b
Aaron Sumner <asumner@mac.com>
2018-05-23 19:05:21 -0700

    Prefer Devise's feature test helpers over Warden's
    https://github.com/everydayrails/everydayrails-rspec-2017/issues/77

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,5 +59,5 @@ RSpec.configure do |config|
   # Use Devise helpers in tests
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include RequestSpecHelper, type: :request
-  config.include Warden::Test::Helpers
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
