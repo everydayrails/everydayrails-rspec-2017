ba36f23ca08db48f6574048cf3b03f3f32a9ce0f
Aaron Sumner <asumner@mac.com>
2018-06-12 21:29:12 -0700

    Add feature for password mismatch

--- features/create_account.feature
+++ features/create_account.feature
@@ -5,3 +5,8 @@ Feature: Create account
     Given I am a new user
     When I register for an account
     Then I am a registered user
+
+  Scenario: Password and password confirmation don't match
+    Given I am a new user
+    When I register with a mismatched password
+    Then I am asked to fix the mismatched password
--- features/step_definitions/step_definitions.rb
+++ features/step_definitions/step_definitions.rb
@@ -12,8 +12,19 @@ When("I register for an account") do
   click_button "Sign up"
 end
 
+When("I register with a mismatched password") do
+  click_link "Sign up"
+  fill_in "Password", with: "secret123!"
+  fill_in "Password confirmation", with: "secret124!"
+  click_button "Sign up"
+end
+
 Then("I am a registered user") do
    expect(page).to have_content "Welcome! You have signed up successfully."
    expect(page).to have_link "Test User", href: edit_user_registration_path
    expect(page).to have_link "Sign Out", href: destroy_user_session_path
+end
+
+Then("I am asked to fix the mismatched password") do
+  expect(page).to have_content "Password confirmation doesn't match Password"
 end
