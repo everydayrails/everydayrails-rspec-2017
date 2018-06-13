cb230d2953bd13939463a501504131d4dbf75b77
Aaron Sumner <asumner@mac.com>
2018-06-12 21:12:15 -0700

    Start step definitions for "create account" feature

---
+++ features/step_definitions/step_definitions.rb
@@ -0,0 +1,11 @@
+Given("I am a new user") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I register for an account") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am a registered user") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
