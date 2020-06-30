0aa4d3fa66dd0e25926fb197d56f8b621f237577
hsiaoshihying <hsiao.shihying@moneyforward.co.jp>
2020-06-30 09:58:12 +0900

    chapter2 setup

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
+  gem 'rspec-rails', '~> 3.6.0'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.13.0'
   gem 'selenium-webdriver'
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,12 @@ module Projects
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
