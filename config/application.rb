b8b4b6508f1b322246aede0d758dab12ac8344ba
naokinoid <naoki.itou.0322@gmail.com>
2021-09-15 23:23:13 +0900

    第２章　セットアップ完了

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Projects
 
     config.generators do |g|
       g.test_framework :rspec,
+        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,8 +6,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'capybara/rspec'
-require 'paperclip/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -22,12 +20,16 @@ require 'paperclip/matchers'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
-# Checks for pending migration and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
-
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -56,24 +58,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  # Use Devise helpers in tests
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include RequestSpecHelper, type: :request
-  config.include Devise::Test::IntegrationHelpers, type: :system
-
-  # Add support for Paperclip's Shoulda matchers
-  config.include Paperclip::Shoulda::Matchers
-
-  # Clean up file uploads when test suite is finished
-  config.after(:suite) do
-    FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_uploads/"])
-  end
-end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
 end
